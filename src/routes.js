b79b8ec58c4454bb568a761805ea17a440579c7e
AllanRolli <papitoallan@gmail.com>
2019-09-12 00:59:41 -0300

    Creation of FileController and route atualization

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,9 @@ const upload = require('multer')(multerConfig)
 const routes = express.Router()
 
 const UserController = require('./app/controllers/UserController')
+const FileController = require('./app/controllers/FileController')
 
+routes.get('/files/:files', FileController.show)
 routes.post('/fornecedores', upload.single('image'), UserController.create)
 
 module.exports = routes
